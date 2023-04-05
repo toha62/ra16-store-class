13c718464351495c15ae61d7c534a3341a242e2a
Anton Trifonov <toha62@inbox.ru>
2023-04-05 22:16:13 +0300

    add class

--- src/ShopItemFunc.jsx
+++ src/ShopItemFunc.jsx
@@ -1,18 +1,28 @@
-export default function ShopItemFunc({ item }) {
-  return (
-    <div className="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div className="description">
-        {item.descriptionFull}
+import React from 'react';
+
+export default class ShopItemFunc extends React.Component {
+  constructor(props) {
+    super(props);
+    this.item = props.item;
+  } 
+
+  render() {
+    return (
+      <div className="main-content">
+        <h2>{this.item.brand}</h2>
+        <h1>{this.item.title}</h1>
+        <h3>{this.item.description}</h3>
+        <div className="description">
+          {this.item.descriptionFull}
+        </div>
+        <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{this.item.currency + this.item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
       </div>
-      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
-      <div className="divider"></div>
-      <div className="purchase-info">
-        <div className="price">{item.currency + item.price}</div>
-        <button>Добавить в корзину</button>
-      </div>
-    </div>
-  )
+    )
+  }
+  
 }
